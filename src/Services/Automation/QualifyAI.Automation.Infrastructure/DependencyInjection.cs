09f5dc7a640c989a57020b57273ebfc9589b7442
burimameti <33432537+burimameti@users.noreply.github.com>
2026-08-27 00:13:59 +0200

    refactor(automation): register unit of work

--- src/Services/Automation/QualifyAI.Automation.Infrastructure/DependencyInjection.cs
+++ src/Services/Automation/QualifyAI.Automation.Infrastructure/DependencyInjection.cs
@@ -1,16 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QualifyAI.Automation.Application.Abstractions.Persistence;
 using QualifyAI.Automation.Domain.AutomationDefinitions;
 using QualifyAI.Automation.Infrastructure.Persistence;
 using QualifyAI.Automation.Infrastructure.Persistence.Repositories;
+
 namespace QualifyAI.Automation.Infrastructure;
+
 public static class DependencyInjection
 {
-    public static IServiceCollection AddAutomationInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddAutomationInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        services.AddDbContext<AutomationDbContext>(o => o.UseSqlServer(configuration.GetConnectionString("AutomationDb")));
-        services.AddScoped<IAutomationDefinitionRepository,AutomationDefinitionRepository>();
+        services.AddDbContext<AutomationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("AutomationDb")));
+
+        services.AddScoped<IAutomationDefinitionRepository, AutomationDefinitionRepository>();
+        services.AddScoped<IAutomationUnitOfWork, AutomationUnitOfWork>();
         return services;
     }
 }
