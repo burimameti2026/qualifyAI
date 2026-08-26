c1aa9018b54f58e68596d8308424006fadec82e4
burimameti <33432537+burimameti@users.noreply.github.com>
2026-08-26 23:36:33 +0200

    refactor(identity): register persistence abstractions cleanly

--- src/Services/Identity/QualifyAI.Identity.Infrastructure/DependencyInjection.cs
+++ src/Services/Identity/QualifyAI.Identity.Infrastructure/DependencyInjection.cs
@@ -2,63 +2,82 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QualifyAI.Identity.Application.Abstractions.Persistence;
 using QualifyAI.Identity.Application.Authentication;
 using QualifyAI.Identity.Infrastructure.Authentication;
 using QualifyAI.Identity.Infrastructure.Identity;
 using QualifyAI.Identity.Infrastructure.Persistence;
+using QualifyAI.Identity.Infrastructure.Persistence.Repositories;
 using QualifyAI.Identity.Infrastructure.Tenants;
 
 namespace QualifyAI.Identity.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddIdentityInfrastructure(this IServiceCollection services,IConfiguration configuration)
+    public static IServiceCollection AddIdentityInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        services.AddDbContext<IdentityDbContext>(o =>
+        services.AddDbContext<IdentityDbContext>(options =>
         {
-            o.UseSqlServer(configuration.GetConnectionString("IdentityDb"));
-            o.UseOpenIddict();
+            options.UseSqlServer(configuration.GetConnectionString("IdentityDb"));
+            options.UseOpenIddict();
         });
 
-        services.AddIdentityCore<ApplicationUser>(o =>
-        {
-            o.Password.RequiredLength=10;
-            o.Password.RequireDigit=true;
-            o.Password.RequireLowercase=true;
-            o.Password.RequireUppercase=true;
-            o.Password.RequireNonAlphanumeric=true;
-            o.Lockout.MaxFailedAccessAttempts=5;
-            o.Lockout.DefaultLockoutTimeSpan=TimeSpan.FromMinutes(15);
-            o.User.RequireUniqueEmail=false; // uniqueness is enforced per tenant.
-        })
-        .AddRoles<ApplicationRole>()
-        .AddEntityFrameworkStores<IdentityDbContext>()
-        .AddSignInManager()
-        .AddDefaultTokenProviders();
+        services
+            .AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+                options.User.RequireUniqueEmail = false;
+            })
+            .AddRoles<ApplicationRole>()
+            .AddEntityFrameworkStores<IdentityDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
 
         services.AddOpenIddict()
-            .AddCore(o => o.UseEntityFrameworkCore().UseDbContext<IdentityDbContext>())
-            .AddServer(o =>
+            .AddCore(options =>
+                options.UseEntityFrameworkCore().UseDbContext<IdentityDbContext>())
+            .AddServer(options =>
             {
-                o.SetIssuer(new Uri(configuration["Identity:Issuer"] ?? "http://identity-api:8080"));
-                o.SetTokenEndpointUris("/connect/token");
-                o.AllowPasswordFlow();
-                o.AllowRefreshTokenFlow();
-                o.RegisterScopes("qualifyai-api","openid","profile","email","offline_access");
-                o.DisableAccessTokenEncryption();
-                o.AddDevelopmentEncryptionCertificate();
-                o.AddDevelopmentSigningCertificate();
-                o.UseAspNetCore().EnableTokenEndpointPassthrough();
+                options.SetIssuer(new Uri(
+                    configuration["Identity:Issuer"] ?? "http://identity-api:8080"));
+                options.SetTokenEndpointUris("/connect/token");
+                options.AllowPasswordFlow();
+                options.AllowRefreshTokenFlow();
+                options.RegisterScopes(
+                    "qualifyai-api",
+                    "openid",
+                    "profile",
+                    "email",
+                    "offline_access");
+                options.DisableAccessTokenEncryption();
+                options.AddDevelopmentEncryptionCertificate();
+                options.AddDevelopmentSigningCertificate();
+                options.UseAspNetCore().EnableTokenEndpointPassthrough();
             })
-            .AddValidation(o =>
+            .AddValidation(options =>
             {
-                o.UseLocalServer();
-                o.UseAspNetCore();
+                options.UseLocalServer();
+                options.UseAspNetCore();
             });
 
-        services.AddScoped<IAccountService,AccountService>();
-        services.AddHttpClient<ITenantDirectoryClient,TenantDirectoryClient>(client =>
-            client.BaseAddress=new Uri(configuration["Services:TenantManagement"] ?? "http://business-api:8080/internal"));
+        services.AddScoped<ITenantRepository, TenantRepository>();
+        services.AddScoped<ILicenseRepository, LicenseRepository>();
+        services.AddScoped<IIdentityUnitOfWork, IdentityUnitOfWork>();
+        services.AddScoped<IAccountService, AccountService>();
+
+        services.AddHttpClient<ITenantDirectoryClient, TenantDirectoryClient>(client =>
+        {
+            client.BaseAddress = new Uri(
+                configuration["Services:TenantManagement"] ?? "http://business-api:8080/internal");
+        });
 
         services.AddHostedService<IdentityBootstrapHostedService>();
         return services;
