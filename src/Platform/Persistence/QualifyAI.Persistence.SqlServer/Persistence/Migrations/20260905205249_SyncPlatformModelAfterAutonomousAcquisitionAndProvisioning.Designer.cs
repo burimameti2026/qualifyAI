91988bf9fd2547aafe83716c93d100ed2f47bb09
Burim Ameti <burimameti2001@gmail.com>
2026-09-05 23:04:52 +0200

    conflict ICPProfiles commit

--- src/Platform/Persistence/QualifyAI.Persistence.SqlServer/Persistence/Migrations/20260905205249_SyncPlatformModelAfterAutonomousAcquisitionAndProvisioning.Designer.cs
+++ src/Platform/Persistence/QualifyAI.Persistence.SqlServer/Persistence/Migrations/20260905205249_SyncPlatformModelAfterAutonomousAcquisitionAndProvisioning.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QualifyAI.Persistence.SqlServer;
 
@@ -11,9 +12,11 @@ using QualifyAI.Persistence.SqlServer;
 namespace QualifyAI.Persistence.SqlServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260905205249_SyncPlatformModelAfterAutonomousAcquisitionAndProvisioning")]
+    partial class SyncPlatformModelAfterAutonomousAcquisitionAndProvisioning
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
