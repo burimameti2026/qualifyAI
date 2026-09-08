52b67a599e9bfdb94e31782bebba27728a049300
burimameti2026 <burimameti44@gmail.com>
2026-09-08 22:33:29 +0200

    feat(real-workspace): register workspace orchestration endpoints

--- src/Platform/Presentation/QualifyAI.Api/Program.cs
+++ src/Platform/Presentation/QualifyAI.Api/Program.cs
@@ -16,22 +16,15 @@ using QualifyAI.Persistence.SqlServer.Queries;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-
 builder.Services.AddMemoryCache();
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
-    typeof(DashboardOverviewQueryHandler).Assembly,
-    typeof(ListCompaniesQueryHandler).Assembly));
-
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(DashboardOverviewQueryHandler).Assembly, typeof(ListCompaniesQueryHandler).Assembly));
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TenantValidationBehavior<,>));
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PermissionAuthorizationBehavior<,>));
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LicenseEntitlementBehavior<,>));
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ModuleEntitlementBehavior<,>));
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
 builder.Services.AddBusinessInfrastructure(builder.Configuration, builder.Environment.IsDevelopment());
 builder.Services.AddPlatformModules(builder.Configuration);
-
 builder.Services.AddScoped<IRequestSecurityContext, BusinessRequestSecurityContext>();
 builder.Services.AddScoped<IAuthorizationHandler, ModuleAuthorizationHandler>();
 builder.Services.AddScoped<LeadQualificationService>();
@@ -39,57 +32,36 @@ builder.Services.AddScoped<WorkflowEngine>();
 builder.Services.AddScoped<SlaService>();
 builder.Services.AddScoped<KnowledgeGapService>();
 builder.Services.AddScoped<BillingService>();
-
 builder.Services.AddHttpClient<IIntegrationProvider, GenericWebhookIntegration>();
-
 builder.Services.Configure<RevenueAutomationOptions>(builder.Configuration.GetSection("RevenueAutomation"));
 builder.Services.AddHostedService<RevenueAutomationWorker>();
-
 builder.Services.AddHostedService<AcquisitionCampaignWorker>();
-
 builder.Services.Configure<AutomationSchedulerOptions>(builder.Configuration.GetSection("AutomationScheduler"));
 builder.Services.AddHostedService<AutomationSchedulerWorker>();
-
 builder.Services.Configure<AutomationRetryOptions>(builder.Configuration.GetSection("AutomationRetry"));
 builder.Services.AddHostedService<AutomationRetryWorker>();
-
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(o =>
 {
     o.SwaggerDoc("v1", new OpenApiInfo { Title="QualifyAI Business API", Version="v1" });
-    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Type=SecuritySchemeType.Http,
-        Scheme="bearer",
-        BearerFormat="JWT"
-    });
+    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { Type=SecuritySchemeType.Http, Scheme="bearer", BearerFormat="JWT" });
 });
-
 builder.Services.AddQualifyAiResourceServer(builder.Configuration);
-
-builder.Services.AddCors(o => o.AddDefaultPolicy(p => p
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .AllowAnyOrigin()));
+builder.Services.AddCors(o => o.AddDefaultPolicy(p => p.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
 
 var app = builder.Build();
-
 app.MapDefaultEndpoints();
-
 app.UseCors();
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseAuthentication();
 app.UseMiddleware<TenantMiddleware>();
 app.UseMiddleware<TenantEntitlementEnforcementMiddleware>();
 app.UseMiddleware<TenantAccessMiddleware>();
 app.UseMiddleware<BillingQuotaMiddleware>();
 app.UseAuthorization();
-
 app.MapControllers();
 app.MapTenantLifecycle();
 app.MapBillingWebhooks();
@@ -100,20 +72,17 @@ app.MapBillingHardeningVerification();
 app.MapAutonomousAcquisition();
 app.MapAutonomousAcquisitionVerification();
 app.MapAutonomousAcquisitionE2e();
+app.MapRealWorkspace();
 app.MapHub<ConversationHub>("/hubs/conversations");
 app.MapPublicChat();
 app.MapExtendedAdmin();
 app.MapPlatformModules();
-
 using(var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     await db.Database.MigrateAsync();
     await db.EnsureBillingSchemaAsync();
     await scope.ServiceProvider.MigratePlatformModuleDatabasesAsync();
-
-    if(builder.Configuration.GetValue<bool>("DemoSeed:Enabled"))
-        await scope.ServiceProvider.GetRequiredService<DemoSeeder>().SeedAsync();
+    if(builder.Configuration.GetValue<bool>("DemoSeed:Enabled")) await scope.ServiceProvider.GetRequiredService<DemoSeeder>().SeedAsync();
 }
-
 app.Run();
