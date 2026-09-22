e2e9410cafcfd7395c449203f2a443b104d0cabc
burimameti2026 <burimameti44@gmail.com>
2026-09-22 18:55:37 +0200

    Add hosted service dependencies for development seed

--- src/Platform/Infrastructure/LeadsAI.Infrastructure/DevelopmentSeedService.cs
+++ src/Platform/Infrastructure/LeadsAI.Infrastructure/DevelopmentSeedService.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using LeadsAI.Infrastructure.WorkspacePackages;
